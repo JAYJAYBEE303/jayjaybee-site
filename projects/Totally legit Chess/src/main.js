8fe6d2c021365c87e7600bf5d8c41bb14c9e5484
Bailey <josh.bailey@astonmartinf1.com>
2026-08-21 16:54:58 +0100

    doing a structure change to make room for the new jayjaybee.com/projects page I want to implement

--- projects/Totally legit Chess/src/main.js
+++
@@ -1,9 +0,0 @@
-import { Chess } from '../lib/chess.js';
-import { BoardView } from './board-view.js';
-import { Game } from './game.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const chess = new Chess();
-  const view  = new BoardView(document.getElementById('board'));
-  new Game(chess, view);
-});
